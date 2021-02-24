65a07fca0a974d35f77de3d47a60095273d9df3d
Ruben <rubenzurr@gmail.com>
2021-02-24 10:31:11 +0300

    Now requests results and stores them in cache, also extracts list of movies from it

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import requests
+import requests_cache
+
+requests_cache.install_cache('project_cache')
+
+
+def get_movies_from_tastedive(st):
+    my_param = {'type': 'movies', 'q': st, 'limit': 5}
+    answer = requests.get('https://tastedive.com/api/similar', params=my_param)
+    return answer.json()
+
+
+def extract_movie_titles(tastedive_resp):
+    movie_ls = []
+    for movie in tastedive_resp['Similar']['Results']:
+        movie_ls.append(movie['Name'])
+    return movie_ls
+
+
 if __name__ == '__main__':
-    print('Initial GIT commit')
+    black_panther = get_movies_from_tastedive('Black Panther')
+    print(extract_movie_titles(black_panther))
 
 
